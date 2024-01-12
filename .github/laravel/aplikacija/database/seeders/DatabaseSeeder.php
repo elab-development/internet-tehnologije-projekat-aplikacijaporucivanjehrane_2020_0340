3d8d7bc4c771689c03bf78ba0f34f3d04e139025
petricz01 <zeljana.petric19@gmail.com>
2024-01-12 18:42:53 +0100

    Izmena u DatabaseSeeder-u

--- .github/laravel/aplikacija/database/seeders/DatabaseSeeder.php
+++ .github/laravel/aplikacija/database/seeders/DatabaseSeeder.php
@@ -18,5 +18,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserSeeder::class,
+            RestoranSeeder::class,
+            KategorijaSeeder::class,
+            ProizvodSeeder::class,
+            NarudzbinaSeeder::class,
+            NarudbinaProizvodSeeder::class,
+            RestoranKategorijaSeeder::class
+        ]);
     }
 }
