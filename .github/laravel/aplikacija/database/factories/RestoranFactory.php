29f4285ea6e66b12fdc46a3f486969cd665ee226
petricz01 <zeljana.petric19@gmail.com>
2024-01-12 17:45:27 +0100

    Restoran Factory

---
+++ .github/laravel/aplikacija/database/factories/RestoranFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Restoran>
+ */
+class RestoranFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'naziv' => $this->faker->company,          
+            'adresa' => $this->faker->address,           
+            'opis' => $this->faker->sentence,             
+            'ocena' => $this->faker->randomFloat(2, 1, 5), 
+            'email' => $this->faker->unique()->safeEmail(),
+        ];
+    }
+}
