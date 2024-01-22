5eb5954aaed311a5262bf9dccc76b6bef129301f
SaraRis <sr20200340@student.fon.bg.ac.rs>
2024-01-22 15:27:37 +0100

    Izmenjena react aplikacija

---
+++ .github/front/poruci/src/komponente/Restoran.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Restoran = ({ restoran })  => {
+  return (
+    <div className='restoranContainer'>
+    <div className='slikaContainer'>
+      <img src={restoran.slika} alt="" className='slika'/>
+    </div>
+    <div className='infoContainer'>
+      <h2>{restoran.naziv}</h2>
+      <p>Ocena: {restoran.ocena}</p>
+      <p>Opis: {restoran.opis}</p>
+      <p>Adresa: {restoran.adresa}</p>
+      <p>E-mail: {restoran.email}</p>
+    </div>
+  </div>
+  )
+}
+
+export default Restoran
