fa34bbf4ad7cbef79d03f6356575a0c784d30085
petricz01 <zeljana.petric19@gmail.com>
2024-01-30 22:29:23 +0100

    Izmenjen App.js

--- .github/front/poruci/src/App.js
+++ .github/front/poruci/src/App.js
@@ -6,15 +6,17 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import SviRestorani from './komponente/SviRestorani';
+import SviProizvodi from './komponente/SviProizvodi';
+import Login from './komponente/Login';
+import Register from './komponente/Register';
 
 
 function App() {
   const [restorani, setRestorani] = useState(null);
-  
 
   useEffect(()=>{
     const fetchData = async () => {
-    const response = await axios.get('./restoraniData.json');
+    const response = await axios.get('http://127.0.0.1:8000/api/restorani');
     console.log(response.data.restorani);
     setRestorani(response.data.restorani);
     }
@@ -31,10 +33,14 @@ function App() {
           <Home/>
         }/>
 
-      <Route path='/rostilj' element = {
-          <SviRestorani restorani={restorani}/>
-        }/>
+      <Route path='/rostilj' element={<SviRestorani restorani={restorani} kategorija='rostilj' />} />
+      <Route path='/poslastice' element={<SviRestorani restorani={restorani} kategorija='poslastice' />} />
+      <Route path='/pica' element={<SviRestorani restorani={restorani} kategorija='pica' />} />
       
+      <Route path="/proizvodi/:restoranId" element={<SviProizvodi />} />
+
+      <Route path='/login' element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
