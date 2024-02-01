8090ca077d6002a859deabc908c190cb54509b68
petricz01 <zeljana.petric19@gmail.com>
2024-02-01 13:46:26 +0100

    Paginacija

--- .github/front/poruci/src/komponente/SviRestorani.jsx
+++ .github/front/poruci/src/komponente/SviRestorani.jsx
@@ -3,45 +3,128 @@ import Restoran from './Restoran';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+
+const kategorije = {
+  rostilj: 1,
+  poslastice: 2,
+  pica: 3,
+
+}
+
+
+
+
 
 function SviRestorani({ kategorija }) {
-  const [restorani, setRestorani] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 2;
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
-      let kategorija_id = 0;
-
-      if (kategorija === 'rostilj') {
-        kategorija_id = 1;
-      } else if (kategorija === 'poslastice') {
-        kategorija_id = 2;
-      } else if (kategorija === 'pica') {
-        kategorija_id = 3;
-      }
 
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://127.0.0.1:8000/api/restorani-za-kategoriju/${kategorija_id}`);
+        const response = await axios.get(`http://127.0.0.1:8000/api/restorani-za-kategoriju/${kategorije[kategorija]}`);
         console.log(response.data.podaci);
-        setRestorani(response.data.podaci);
+        
+        
+        const filtered = response.data?.podaci.filter(
+          (restoran) =>
+            restoran.naziv &&
+            restoran.naziv.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      
+        setFilteredRestaurants(filtered);
       } catch (error) {
         console.error('Greška pri dohvatanju restorana za kategoriju:', error);
       }
     };
 
     fetchData();
-  }, [kategorija]);
+  }, [kategorija, searchTerm]);
   // const filtriraniRestorani = restorani?.filter(r => r.kategorija_naziv.toLowerCase() === kategorija.toLowerCase());
   // console.log(filtriraniRestorani);
+
+
+const pageCount = filteredRestaurants
+  ? Math.ceil(filteredRestaurants.length / itemsPerPage)
+  : 0;
+
+const displayRestaurants = () => {
+  if (
+    !Array.isArray(filteredRestaurants) ||
+    filteredRestaurants.length === 0
+  ) {
+    return <p>No restaurants to display.</p>;
+  }
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
   return (
     <div>
-      {restorani?.map((r) => (
+      {filteredRestaurants.slice(startIndex, endIndex).map((r) => (
+        // <Restoran key={r.id} restoran={r} />
+         <Link to={`/proizvodi/${r.id}`} key={r.id}>
+         <Restoran restoran={r} />
+       </Link>
+      ))}
+    </div>
+  );
+};
+
+const handlePageClick = ({ selected }) => {
+  setCurrentPage(selected);
+};
+
+const handleNextClick = () => {
+  setCurrentPage((prevPage) => prevPage + 1);
+};
+
+const handlePrevClick = () => {
+  setCurrentPage((prevPage) => prevPage - 1);
+};
+
+  return (
+    
+    <div>
+      <input
+        type="text"
+        placeholder="Pronađi restoran (naziv)"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{
+          margin: "10px",
+          padding: "8px",
+          width: "30%",
+          display: "block",
+          margin: "0 auto",
+        }}
+      />
+      {displayRestaurants()}
+      
+      {/* {restorani?.map((r) => (
         <Link to={`/proizvodi/${r.id}`} key={r.id}>
         <Restoran restoran={r} />
       </Link>
-    
    
         //<Restoran restoran={r} key={r.id} />
-      ))}
+      ))}  */}
+
+      {/* Paginacija */}
+      <ReactPaginate
+  pageCount={pageCount}
+  pageRangeDisplayed={3}
+  marginPagesDisplayed={1}
+  onPageChange={handlePageClick}
+  containerClassName={'pagination'}
+  activeClassName={'active'}
+  previousLabel={<div className="pagination-btn">Prethodna</div>}
+  nextLabel={<div className="pagination-btn">Sledeca</div>}
+/>
     </div>
   
   
