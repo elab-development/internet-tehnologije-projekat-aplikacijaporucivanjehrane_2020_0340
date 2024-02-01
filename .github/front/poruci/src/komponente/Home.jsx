55ceb8ce230b8a10168d4ec8dcf11e23deeec913
SaraRis <sr20200340@student.fon.bg.ac.rs>
2024-02-01 13:54:38 +0100

    Izmene u App.css

--- .github/front/poruci/src/komponente/Home.jsx
+++ .github/front/poruci/src/komponente/Home.jsx
@@ -4,35 +4,35 @@ import { Link } from 'react-router-dom';
 
 
 
-const Home = ({ restorani }) => {
-  const kategorije = {
-    rostilj: 1,
-    poslastice: 2,
-    pica: 3,
-  };
+const Home = () => {
+  //  const kategorije = {
+  //    rostilj: 1,
+  //    poslastice: 2,
+  //    pica: 3,
+  //  };
 
   return (
     <div className="home-container">
       <div className="image-container">
         <img src="https://res.cloudinary.com/grand-canyon-university/image/fetch/w_750,h_564,c_fill,g_faces,q_auto,f_auto/https://www.gcu.edu/sites/default/files/media/images/Blog/Content%20Campaigns/shutterstock_1383065213.jpg" alt="Food Categories" className='img' />
         <div className="category-overlay">
-           <Link to = '/rostilj'>
-          <div className="category-text">#rostilj</div>
+          <Link to = '/rostilj'>
+          <div className="category-text">rostilj</div>
           </Link>
           <Link to='/poslastice/'>
-            <div className="category-text">#poslastice</div>
+            <div className="category-text">poslastice</div>
           </Link>
           <Link to='/pica'>
-            <div className="category-text">#pica</div>
+            <div className="category-text">pica</div>
           </Link>
           <Link to = '/rostilj'>
-          <div className="category-text">#rostilj</div>
+          <div className="category-text">rostilj</div>
           </Link>
           <Link to='/poslastice/'>
-            <div className="category-text">#poslastice</div>
+            <div className="category-text">poslastice</div>
           </Link>
           <Link to='/pica'>
-            <div className="category-text">#pica</div>
+            <div className="category-text">pica</div>
           </Link>
         </div>
       </div>
